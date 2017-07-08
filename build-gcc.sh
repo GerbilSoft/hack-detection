09c78183350e417ae542aa99af668e408cb265fb
David Korth <gerbilsoft@gerbilsoft.com>
2017-07-07 20:31:59 -0400

    build-gcc.sh: Split debugging symbols.
    Set TARGET to the gcc prefix instead of specifying CC.
    This allows it to be set for objcopy and strip.

--- build-gcc.sh
+++ build-gcc.sh
@@ -15,18 +15,30 @@ if [ -z "${ISWIN}" ]; then
 			;;
 	esac
 fi
-if [ -z "${CC}" ]; then
+if [ ! -z "${TARGET}" ]; then
+	CC=${TARGET}-gcc
+	OBJCOPY=${TARGET}-objcopy
+	STRIP=${TARGET}-strip
+else
 	CC=gcc
+	OBJCOPY=objcopy
+	STRIP=strip
 fi
 
 CFLAGS="-O2 -Wall -Wextra -std=c99"
 EXE=hack-detection
+DEBUG=hack-detection.debug
 if [ "${ISWIN}" == "1" ]; then
-	EXE=hack-detection.exe
+	EXE=${EXE}.exe
 fi
 
 echo CC=${CC}
 echo CFLAGS=${CFLAGS}
 echo EXE=${EXE}
+echo DEBUG=${DEBUG}
+set -e
 set -v
 ${CC} ${CFLAGS} -o ${EXE} hack-detection.c
+${OBJCOPY} --only-keep-debug ${EXE} ${DEBUG}
+${STRIP} ${EXE}
+${OBJCOPY} --add-gnu-debuglink=${DEBUG} ${EXE}
