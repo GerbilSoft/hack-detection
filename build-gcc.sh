be38ff65e1a11c384f9fe73e9f5ceabfdcfd836f
David Korth <gerbilsoft@gerbilsoft.com>
2017-07-07 22:19:24 -0400

    build-gcc.sh: Compile and link in the Windows resource file.

--- build-gcc.sh
+++ build-gcc.sh
@@ -19,10 +19,12 @@ if [ ! -z "${TARGET}" ]; then
 	CC=${TARGET}-gcc
 	OBJCOPY=${TARGET}-objcopy
 	STRIP=${TARGET}-strip
+	RC=${TARGET}-windres
 else
 	CC=gcc
 	OBJCOPY=objcopy
 	STRIP=strip
+	RC=windres
 fi
 
 CFLAGS="-O2 -Wall -Wextra -std=c99"
@@ -34,14 +36,22 @@ if [ "${ISWIN}" == "1" ]; then
 	EXE="${EXE}.exe"
 fi
 
-echo CC=${CC}
-echo CFLAGS=${CFLAGS}
-echo LDFLAGS=${LDFLAGS}
-echo EXE=${EXE}
-echo DEBUG=${DEBUG}
 set -e
+if [ "${ISWIN}" == "1" ]; then
+	# Compile in multiple steps, since we have a resource file.
+	echo ${CC} -c ${CFLAGS} -o hack-detection.o hack-detection.c
+	${CC} -c ${CFLAGS} -o hack-detection.o hack-detection.c
+	echo ${RC} resource.rc resource.o
+	${RC} resource.rc resource.o
+	echo ${CC} ${CFLAGS} ${LDFLAGS} -o ${EXE} hack-detection.o resource.o
+	${CC} ${CFLAGS} ${LDFLAGS} -o ${EXE} hack-detection.o resource.o
+else
+	# Compile and link in one step.
+	echo ${CC} ${CFLAGS} -o ${EXE} hack-detection.c ${LDFLAGS}
+	${CC} ${CFLAGS} -o ${EXE} hack-detection.c ${LDFLAGS}
+fi
+
 set -v
-${CC} ${CFLAGS} -o ${EXE} hack-detection.c ${LDFLAGS}
 ${OBJCOPY} --only-keep-debug ${EXE} ${DEBUG}
 ${STRIP} ${EXE}
 ${OBJCOPY} --add-gnu-debuglink=${DEBUG} ${EXE}
