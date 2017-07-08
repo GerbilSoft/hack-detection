6f146ab411d38359401c4945d9a571c487ee9cd6
David Korth <gerbilsoft@gerbilsoft.com>
2017-07-07 20:28:03 -0400

    build-gcc.sh: Detect Windows/MinGW and append a ".exe" extension.
    Build with optimizations. (-O2)

--- build-gcc.sh
+++ build-gcc.sh
@@ -1,2 +1,32 @@
 #!/bin/sh
-gcc -Wall -Wextra -std=c99 -o hack-detection hack-detection.c
+if [ -z "${ISWIN}" ]; then
+	ISWIN=0
+	case "$OSTYPE" in
+		cygwin)
+			ISWIN=1
+			;;
+		msys)
+			ISWIN=1
+			;;
+		win32)
+			ISWIN=1
+			;;
+		*)
+			;;
+	esac
+fi
+if [ -z "${CC}" ]; then
+	CC=gcc
+fi
+
+CFLAGS="-O2 -Wall -Wextra -std=c99"
+EXE=hack-detection
+if [ "${ISWIN}" == "1" ]; then
+	EXE=hack-detection.exe
+fi
+
+echo CC=${CC}
+echo CFLAGS=${CFLAGS}
+echo EXE=${EXE}
+set -v
+${CC} ${CFLAGS} -o ${EXE} hack-detection.c
