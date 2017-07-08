fdcef010624f36e81f4e81f0e98e11996deb5fc9
David Korth <gerbilsoft@gerbilsoft.com>
2017-07-07 20:48:47 -0400

    build-msvc.bat: /incremental:no, not /noincremental.

--- build-msvc.bat
+++ build-msvc.bat
@@ -1,1 +1,1 @@
-CL /O2 /MD /EHsc /Zi /Fehack-detection.exe hack-detection.c /link /debug /opt:icf,ref /noincremental
+CL /O2 /MD /EHsc /Zi /Fehack-detection.exe hack-detection.c /link /debug /opt:icf,ref /incremental:no
